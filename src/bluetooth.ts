43dd2ba4272b7c581331605240b35fd117c19023
Kyle Machulis <kyle@nonpolynomial.com>
2017-09-10 16:38:15 -0700

    Add flash info fetching function

--- src/bluetooth.ts
+++ src/bluetooth.ts
@@ -1,8 +1,6 @@
 import * as util from "util";
 import * as noble from "noble";
-
 export class LaunchBluetooth {
-
   private serviceUUID = "88f80580000001e6aace0002a5d5c51b";
   private charUUIDs = { cmd: "88f80583000001e6aace0002a5d5c51b",
                         // sensor: "88f80582-0000-01e6-aace-0002a5d5c51b",
@@ -59,13 +57,19 @@ export class LaunchBluetooth {
     return await util.promisify(this.dataChar.write.bind(this.dataChar))(aData, false);
   }
 
-  public async CommandWithResponse(aCommand: number, aData: number = 0x00) {
+  public async CommandWithResponse(aCommand: number, aData: number = 0x00): Promise<Buffer> {
     const cmd = Buffer.from([aCommand]);
     await this.writeCmd(cmd);
     await this.writeData(Buffer.from([aData]));
     const cmdRet = await this.readCmd();
     const dataRet = await this.readData();
-    console.log(dataRet);
+    return dataRet;
+  }
+
+  // Seems to be used after firmware loading to set machine back to app mode?
+  public async LockAppMode() {
+    await this.writeCmd(Buffer.from([0x0D]));
+    await this.writeData(Buffer.from([0x4F, 0x4B]));
   }
 
   public async GetVersion() {
@@ -74,6 +78,37 @@ export class LaunchBluetooth {
 
   public async GetExecutionMode() {
     return await this.CommandWithResponse(0x03);
+  }
+
+  public async GetFlashInfo() {
+    const flashInfo = await this.CommandWithResponse(0x0A);
+    let i = 0;
+    const is24BitAddressed: boolean = flashInfo.length === 9;
+    const writeBlockCommandSize = is24BitAddressed ? 3 : 2;
+    const flashEraseValue = is24BitAddressed ? flashInfo.readUInt8(i) : 0xFF;
+    if (is24BitAddressed) {
+      i += 1;
+    }
+    const addressIncrement = (flashInfo.readUInt8(i) & 0xF0) >> 4;
+    const wordSize = flashInfo.readUInt8(i) & 0x0F;
+    i += 1;
+    const programLength = flashInfo.readUInt16BE(i);
+    i += 2;
+    const programBase = is24BitAddressed ?
+      // Ugh no 24-bit reads in node's buffer class.
+      flashInfo.readUInt8(i) | (flashInfo.readUInt16BE(i + 1) << 8) :
+      flashInfo.readUInt16BE(i);
+    i += is24BitAddressed ? 3 : 2;
+    const rowSize = flashInfo.readUInt16BE(i);
+    return {
+      writeBlockCommandSize,
+      flashEraseValue,
+      programLength,
+      wordSize,
+      addressIncrement,
+      programBase,
+      rowSize,
+    };
   }
 
   public async Initialize() {
